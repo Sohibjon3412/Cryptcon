755c28d1bdcd2c96c30a3cd0c5933815537d171c
Sohibjon3412 <Sohibjon34122@gmail.com>
2021-10-11 02:49:53 -0700

    commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let darkMod = document.querySelector(".hero-menu");
+let elModal = document.querySelector(".modal");
+
+darkMod.addEventListener("click",function() {
+  elModal.classList.toggle("modal__height");
+});
+
